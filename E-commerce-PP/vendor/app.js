62d7c30400195ee581b1243b189726455c5740b4
skykta <60268085+skykta@users.noreply.github.com>
2020-09-19 21:52:26 +0800

    update

--- E-commerce-PP/vendor/app.js
+++
@@ -1,8 +0,0 @@
-$('.text-box input').on('focus', function () {
-    $(this).addClass('focus');
-});
-
-$('.text-box input').on('blur', function () {
-    if ($(this).val() == '') 
-    $(this).removeClass('focus');
-});
